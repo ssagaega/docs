475b6f45e5ed2774dd949a8856474f094b579a38
ssagaega <45482899+ssagaega@users.noreply.github.com>
2018-12-11 03:09:05 +0900

    Update detail.js

--- js/detail.js
+++ js/detail.js
@@ -28,14 +28,9 @@ function filterById( selected ) {
       }
     });
   }
-}; 
+};
 
-function telegram() {
-  var url = "https://api.telegram.org/bot572309405:AAFIvvAIGOciZcnkzQHPqUaRoWPNebvULmg/sendMessage?chat_id=59644837&text=" + encodeURI( "[" + detailApp.item.id + "] [" + detailApp.item.title + "] \n" + detailApp.errorReportMessage );
-  $.get( url, function( data, status ) {
-    createAlert( '정보 개선에 참여해 주셔서 감사합니다.', 'alert-success')
-  });
-}
+
 
 var detailApp = new Vue({
   el: '#detailApp',
@@ -87,82 +82,3 @@ var detailApp = new Vue({
     }
   }
 });
-
-function createAlert(title, summary, details, severity, dismissible, autoDismiss, appendToId) {
-  var iconMap = {
-    info: "fa fa-info-circle",
-    success: "fa fa-thumbs-up",
-    warning: "fa fa-exclamation-triangle",
-    danger: "fa ffa fa-exclamation-circle"
-  };
-
-  var iconAdded = false;
-
-  var alertClasses = ["alert", "animated", "flipInX"];
-  alertClasses.push("alert-" + severity.toLowerCase());
-
-  if (dismissible) {
-    alertClasses.push("alert-dismissible");
-  }
-
-  var msgIcon = $("<i />", {
-    "class": iconMap[severity] // you need to quote "class" since it's a reserved keyword
-  });
-
-  var msg = $("<div />", {
-    "class": alertClasses.join(" ") // you need to quote "class" since it's a reserved keyword
-  });
-
-  if (title) {
-    var msgTitle = $("<h4 />", {
-      html: title
-    }).appendTo(msg);
-    
-    if(!iconAdded){
-      msgTitle.prepend(msgIcon);
-      iconAdded = true;
-    }
-  }
-
-  if (summary) {
-    var msgSummary = $("<strong />", {
-      html: summary
-    }).appendTo(msg);
-    
-    if(!iconAdded){
-      msgSummary.prepend(msgIcon);
-      iconAdded = true;
-    }
-  }
-
-  if (details) {
-    var msgDetails = $("<p />", {
-      html: details
-    }).appendTo(msg);
-    
-    if(!iconAdded){
-      msgDetails.prepend(msgIcon);
-      iconAdded = true;
-    }
-  }
-  
-
-  if (dismissible) {
-    var msgClose = $("<span />", {
-      "class": "close", // you need to quote "class" since it's a reserved keyword
-      "data-dismiss": "alert",
-      html: "<i class='fa fa-times-circle'></i>"
-    }).appendTo(msg);
-  }
-  
-  $('#' + appendToId).prepend(msg);
-  
-  if(autoDismiss){
-    setTimeout(function() {
-      msg.addClass("flipOutX");
-      setTimeout(function(){
-        msg.remove();
-      },500);
-    }, 500);
-  }
-}
